c3cf5697692687942f5e5c30de9ce98a1f7f880c
joshcamp503 <joshcamp503@gmail.com>
2022-05-15 23:26:16 -0700

    edited about page text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,28 +8,33 @@ const About = () => {
       <h1 className="about-header">ABOUT ME</h1>
       <div className="about-text-container">
         <p className="about-text-1">
-        I’m a React developer living in Portland, OR and I pray at the altar of the holy web trinity… HTML, CSS, and Javascript. Hallelujah! (And if you’ve received a cover letter from me, you may have heard that joke before. Sorry!)  But all jokes aside, I am a serious student of all things web.  
+          I’m Josh Campanella and I make web applications in Portland, OR. I work with the React.js framework and I’m a JavaScript maven.
         </p>
 
         <p className="about-text-2">
-        I have been studying the holy three off and on as a hobby since about 2014 when my interest first sparked.  But about 2 years ago I got serious about making this a full-time gig, and here I am now.  I am fascinated by the power of the web & technology and how these forces will continue to shape society as we know it.
+          Bringing people’s ideas to life is my strength. My skills are particularly tailored to the building process – pixel-perfect development, implementing complex functionality, and providing a seamless user experience.
         </p>
 
         <p className="about-text-3">
-        I can flex any box, await any fetch, and .then(manage any state). I’ve been working primarily with React.js and Javascript and I love how seamless and snappy a web application feels when it’s built on the React.js platform. But that being said, I’ve got the fundamentals and flexibility to quickly switch to a new framework if asked, or even a new language (looking at you, Python & Django).  
+          I pride myself on my attention to detail, my aptitude for figuring things out, and my reliable sense of accountability… and my CSS skills.
         </p>
 
         <p className="about-text-4">
-        On the back end I’ve mostly worked with MongoDB and Node.js with Express, setting up APIs for my various projects. When I’m stuck on something, the first place I turn to is the documentation – MDN, reactjs.com, reactrouter.com, etc.  Sometimes I look to Stack Overflow for guidance, but I find that to be a mixed bag.  And lastly, I have a firm understanding of Git and Github – initializing repos, cloning repos, adds, commits, remotes, checking out branches, merging, etc. 
+          An unquenchable thirst for knowledge drives me, and I have an insatiable appetite for trying new things. In my free time I particularly enjoy gaming, motorcycles, fishing, and learning about web technologies and programming languages.
         </p>
 
         <p className="about-text-5">
-        I’ve spent 4 years of my life working as an independent construction contractor.  I am productive, competent, accountable, and excellent at communicating and working with a team on complex projects with a lot of moving parts. I know how to put my head down and work, and if you’re looking to add someone to your team with a ton of technical talent and all the soft skills to go with it, give me a callback (pun intended), shoot me an email, or submit the contact form below.
+          I did construction for 5 years as an independent contractor and learned first-hand the challenges that business owners face. That experience collaborating with customers and other business owners was invaluable in shaping me into the professional I am today.
         </p>
 
         <p className="about-text-6">
+          If you’re looking for someone to help you or your team, give me a callback, shoot me an email, or submit the contact form below.
+        </p>
+
+        <p className="about-text-7">
           Looking forward to meeting you!
         </p>
+
       </div>
     </div>
   )
