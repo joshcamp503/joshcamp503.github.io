6a47ed24d293984ba038f803b41173b5bbb87055
joshcamp503 <joshcamp503@gmail.com>
2023-04-14 03:51:38 -0700

    Resume.js: updated filepaths to reflect new resume file

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,7 +2,7 @@
 import './Resume.css'
 
 // DOCS
-import ResumePDF from '../../assets/josh-campanella-resume-2022.pdf'
+import ResumePDF from '../../assets/josh-campanella-resume-2023.pdf'
 
 // COMPONENTS
 import { saveAs } from 'file-saver'
@@ -14,9 +14,9 @@ const Resume = () => {
     <Element id="resume" name="resume">
       <div className="resume">
         <h1 className="title">RESUME</h1>
-        <button className="download" onClick={() => {saveAs(ResumePDF, 'josh-campanella-resume-2022.pdf')}}>DOWNLOAD</button>
+        <button className="download" onClick={() => {saveAs(ResumePDF, 'josh-campanella-resume-2023.pdf')}}>DOWNLOAD</button>
         <object data={ResumePDF} type="application/pdf" >Resume not found</object>
-        <button className="download" id="button2" onClick={() => {saveAs(ResumePDF, 'josh-campanella-resume-2022.pdf')}}>DOWNLOAD</button>
+        <button className="download" id="button2" onClick={() => {saveAs(ResumePDF, 'josh-campanella-resume-2023.pdf')}}>DOWNLOAD</button>
       </div>
     </Element>
   )
