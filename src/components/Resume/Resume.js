ddde4f69e21b344f84593f8f79cd892fc44e3e16
joshcamp503 <joshcamp503@gmail.com>
2023-04-14 04:16:33 -0700

    Resume.js/css: styled <object> message for mobile

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -15,8 +15,8 @@ const Resume = () => {
       <div className="resume">
         <h1 className="title">RESUME</h1>
         <button className="download" onClick={() => {saveAs(ResumePDF, 'josh-campanella-resume-2023.pdf')}}>DOWNLOAD</button>
-        <object data={ResumePDF} type="application/pdf" >Resume not found</object>
-        <button className="download" id="button2" onClick={() => {saveAs(ResumePDF, 'josh-campanella-resume-2023.pdf')}}>DOWNLOAD</button>
+        <object data={ResumePDF} type="application/pdf" >Click "download" to view resume</object>
+        <button className="download" id="button2" onClick={() => {saveAs(ResumePDF, 'josh-campanella-resume-202.pdf')}}>DOWNLOAD</button>
       </div>
     </Element>
   )
